f42a271c980b0f76d832b4fe5a4169827832a42c
bySimpson <47668312+bySimpson@users.noreply.github.com>
2021-12-30 23:01:20 +0100

    Update xrandr.sh

--- xrandr.sh
+++ xrandr.sh
@@ -16,11 +16,11 @@ if ! [ -z "$(xrandr --listactivemonitors | grep 'Virtual1~1')" ]; then
 	echo "removing screens"
 	xrandr --delmonitor Virtual1~1
 	xrandr --delmonitor Virtual1~2
-	xrandr --fb 2561x1440
-	xrandr --fb 2560x1440
+        xrandr --fb $(expr $resx + 1)x$resy
+        xrandr --fb "$resx"x"$resy"
 fi
 echo "setting screens"
-xrandr --setmonitor Virtual1~1 $pres1/154x$resy/173+0+0 Virtual1
+xrandr --setmonitor Virtual1~1 $pres1/154x$resy/173+0+0 HDMI-1
 xrandr --setmonitor Virtual1~2 $pres2/155x$resy/173+$pres1+0 none
 xrandr --fb $(expr $resx + 1)x$resy
 xrandr --fb "$resx"x"$resy"
